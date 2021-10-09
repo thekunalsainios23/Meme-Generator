da4eb90a457da32a15199abd177ed19c06970028
thekunalsaini <43953836+thekunalsaini@users.noreply.github.com>
2021-10-09 14:21:03 +0530

    Update script.js

--- script.js
+++ script.js
@@ -27,6 +27,7 @@ function linkGenerator(error, response, body) {
   }
 }
 
+
 function getTopicPage(name, url) {
   request(url, function (error, response, body) {
     if (!error && response.statusCode == 200) {
